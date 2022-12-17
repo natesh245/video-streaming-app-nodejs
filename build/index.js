6a70b926176e758c3437f0a97e3738650c7d44a0
natesh <natesh@nateshs-MacBook-Pro.local>
2022-12-17 13:03:04 +0530

    /video stram endpoint

--- build/index.js
+++ build/index.js
@@ -4,10 +4,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const PORT = 4000;
 const app = (0, express_1.default)();
 app.get("/hello", (req, res) => {
     res.send("Hello");
+});
+app.get("/page", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+app.get("/video", (req, res) => {
+    const range = req.headers.range;
+    if (!range) {
+        return res.status(400).send("Range header is required");
+    }
+    console.log(range);
+    const videoPath = __dirname + "/../assets/videos/pexels-ana-benet-8242842.mp4";
+    const videoSize = fs_1.default.statSync(videoPath).size;
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+    const videoStream = fs_1.default.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
 });
 app.listen(PORT, () => {
     console.log("Listening on port" + PORT);
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,39 @@
-import express, { Request, Response, Application } from "express";
+import express, { Request, Response, Express } from "express";
+import fs from "fs";
 const PORT: number = 4000;
 
-const app: Application = express();
+const app: Express = express();
 
 app.get("/hello", (req: Request, res: Response) => {
   res.send("Hello");
+});
+
+app.get("/page", (req: Request, res: Response) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/video", (req: Request, res: Response) => {
+  const range = req.headers.range;
+  if (!range) {
+    return res.status(400).send("Range header is required");
+  }
+  console.log(range);
+  const videoPath =
+    __dirname + "/../assets/videos/pexels-ana-benet-8242842.mp4";
+  const videoSize = fs.statSync(videoPath).size;
+  const CHUNK_SIZE = 10 ** 6; // 1MB
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+  res.writeHead(206, headers);
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+  videoStream.pipe(res);
 });
 
 app.listen(PORT, () => {
